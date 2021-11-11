95229bc5c2800322d727faa977fa462d83f03f39
n0vaan00 <n0vaan00@students.oamk.fi>
2021-11-11 13:20:14 +0200

    yhteystiedot-footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import './Footer.css';
 
 export default function Footer() {
     return (
         <div>
             <footer className="footer">
                 <div className="container">
+                    <p>PaitaÄssä</p>
+                    <p>Maalimanlaita 666</p>
+                    <p>99663 Hukka</p>
                     <span className="text-muted">This is footer</span>
                 </div>
+                
             </footer>
         </div>
     )
