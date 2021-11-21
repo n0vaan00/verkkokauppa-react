e8f87cdc17a54ab3af13d41d85cb37012e612e06
unknown <n7haat00@student.oamk.fi git config --global user.name>
2021-11-21 18:08:35 +0200

    categorian vaihto

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Link } from "react-router-dom";
 
 
+
 export default function Home({url,category}) {
   const [products, setProducts] = useState([]);
 
@@ -21,7 +22,18 @@ export default function Home({url,category}) {
   }, [])
         return (
             <>
-            <div style={{'padding-top': '100px'}}></div>
+            <div style={{'padding-top': '100px'}}>
+              <h3>Products for</h3>
+              {products.map(product => (
+                <div key={product.trnro}>
+                  
+                  <p>{product.trnimi}</p>
+                    <div>
+                      <img src={url + 'images/' + product.image} alt="" />
+                    </div>
+                  </div>
+              ))}
+            </div>
               <nav>
                 <Link to="/kirjaudu">About</Link>
               </nav>
