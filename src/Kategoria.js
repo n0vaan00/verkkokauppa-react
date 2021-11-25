596f8bd15a91b9b7f4722b0bc07046a0e79b3685
n0vaan00 <n0vaan00@students.oamk.fi>
2021-11-25 16:54:20 +0200

    semirikkinen vedos

---
+++ src/Kategoria.js
@@ -0,0 +1,51 @@
+import React,{useState,useEffect} from 'react'
+import axios from 'axios';
+import {Link} from "react-router-dom";
+
+
+
+export default function Kategoria({url,category,addToCart}) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (category !== null){
+    axios.get(url + 'products/getproducts.php/' + category?.trnro)
+    .then((response) => {
+      const json = response.data;
+      setProducts(json);
+    }).catch (error => {
+      if (error.response === undefined) {
+        alert(error);
+      } else {
+        alert(error.response.data.error);
+      }
+    })
+  }
+  }, [category])
+        return (
+            <>
+            <div style={{'paddingTop': '100px'}}>
+              <h3>Tuotteet ryhmästä: {category?.trnimi}</h3>
+              {products.map(product => (
+                <div key={product.tuotenro}>
+                  <Link to={{
+                    pathname: '/tuote',
+                    state: {
+                      trnro: product.trnro,
+                      trnimi: product.trnimi
+                    }
+                  }}>
+                  <p>{product.tuotenimi}</p>
+                  </Link>
+                  <p>{product.hinta}</p>
+                  {/* <button class="btn btn-primary" type="button" onClick={e => addToCart(product)}>Add to cart</button> */}
+                   {/*} <div>
+                      <img src={url + 'images/' + product.image} alt="" />
+                   </div> */}
+                  </div>
+              ))}
+            </div>
+              
+            </>
+          );
+}
