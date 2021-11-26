318931ed3ca8bcc3a3bb21ee0fb21bf1c835012a
unknown <n7haat00@student.oamk.fi git config --global user.name>
2021-11-26 14:36:12 +0200

    jotain fixailua

--- src/Product.js
+++ src/Product.js
@@ -2,9 +2,10 @@ import React from "react";
 
 export default function Product({product,addToCart}) {
     return (
+        
         <div style={{'padding-top': '100px'}}>
-            {product?.name}
-            <button class="btn btn-primary" type="button" onClick={e => addToCart(product)}>Add to cart</button>
+            {product?.trnimi}
+            <button class="btn btn-primary" type="button" onClick={e => addToCart(product)}>Lisää ostoskoriin</button>
         </div>
     )
 }
