1b56f63adb8dcdc9c33c75fced83af5b55f8d01c
adepade <n7haat00@students.oamk.fi>
2021-12-06 15:52:39 +0200

    uutta mainost

--- src/Home.js
+++ src/Home.js
@@ -23,7 +23,7 @@ export default function Home() {
                     <img
                     id="mainos"
                     className="d-block"
-                    src="mainos2.jpg"
+                    src="mainos4.jpg"
                     alt="Second slide"
                     />
                     <Carousel.Caption>
