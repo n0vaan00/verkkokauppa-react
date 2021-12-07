24502fe203c66d5e431012558dec3541e87e5c15
n0vaan00 <n0vaan00@students.oamk.fi>
2021-12-07 16:37:57 +0200

    css ja alet

--- src/Adminprod.js
+++ src/Adminprod.js
@@ -1,9 +1,11 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import './Kirjaudu.css';
 
     const URL = 'http://localhost/verkkokauppaprojekti-back/products/';
     const SHOWPRODU = 'getproduct.php';
+    const SHOW = 'getcategories.php';
 
 export default function Adminprod() {
 
@@ -13,6 +15,7 @@ export default function Adminprod() {
     const [image, setImage] = useState('');
     const [info, setInfo] = useState('');
     const [cat_id, setCatId] = useState('');
+    const [tryhma, setTryhma] = useState([]);
 
 /*Tuotteiden lisäys ja poisto*/
       function saveprod(e) {
@@ -61,36 +64,56 @@ export default function Adminprod() {
                 alert(error);
               });
           }, [])
-     
+          
+    useEffect(() => {
+            axios.get(URL+SHOW)
+              .then((response) => {
+                setTryhma(response.data);
+              }).catch(error => {
+                alert(error);
+              });
+          }, []) 
+
     return (
 
-    <div className="container">
+    <div className="container" id="main">
     <Link className="nav-link" aria-current="page" to="/admincat">Hallinnoi tuoteryhmiä</Link>
 
     <h3>Lisää tuote</h3>
     <form onSubmit={saveprod}>
       <label>Uusi tuote</label>
-      <input value={name} onChange={e => setName(e.target.value)} placeholder="Uusi tuote"/>
+      <input value={name} onChange={e => setName(e.target.value)} placeholder="Uusi tuote"/><br/>
       <label>Tuotteen hinta</label>
-      <input value={price} onChange={e => setPrice(e.target.value)} placeholder="Tuotteen hinta"/>
+      <input value={price} onChange={e => setPrice(e.target.value)} placeholder="Tuotteen hinta"/><br/>
       <label>Tuotteen kuva</label>
-      <input value={image} onChange={e => setImage(e.target.value)} placeholder="Tuotteen kuva"/>
+      <input value={image} onChange={e => setImage(e.target.value)} placeholder="Tuotteen kuva"/><br/>
       <label>Tuotteen tiedot</label>
-      <input value={info} onChange={e => setInfo(e.target.value)} placeholder="Tuotteen tiedot"/>
+      <input value={info} onChange={e => setInfo(e.target.value)} placeholder="Tuotteen tiedot"/><br/>
       <label>Tuotteen tuoteryhmänumero</label>
-      <input value={cat_id} onChange={e => setCatId(e.target.value)} placeholder="Tuotteen ryhmänumero"/>
+      <input value={cat_id} onChange={e => setCatId(e.target.value)} placeholder="Tuotteen ryhmänumero"/><br/>
       <button>Tallenna</button>
     </form>
     <ol>
       {product?.map(productname =>(
-        <li key={productname.id}>
-         {productname.name},  Hinta: {productname.price}, Kuva: {productname.image}, Tiedot: {productname.info},   Ryhmänumero: {productname.category_id}&nbsp;
+        <li key={productname.id} id="adminlista">
+         {productname.id}. {productname.name},  Hinta: {productname.price}, Kuva: {productname.image}, Tiedot: {productname.info},   Ryhmänumero: {productname.category_id}&nbsp;
           <a href="#" className="delete" onClick={() => removeprod(productname.id)}>
             Delete
           </a>
         </li>
       ))}
     </ol>
+    <br/>
+    <div id="katlist">
+      <h4>Kategoriat</h4>
+    <ol>
+        {tryhma?.map(ryhma =>(
+          <li key={ryhma.id} id="adminlista">
+            {ryhma.id}. {ryhma.name}&nbsp;
+          </li>
+        ))}
+      </ol>
+    </div>
     </div>
 
     )
