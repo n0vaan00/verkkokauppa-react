489f9ff72f78de9d2ea986e1924d1540081318ef
n0vaan00 <n0vaan00@students.oamk.fi>
2021-12-15 15:08:12 +0200

    pistin omani

--- src/Infot.js
+++ src/Infot.js
@@ -6,13 +6,13 @@ export default function Infot() {
             <div id="atte"></div>
             <div id="anu">
                 <h3>Anu Väyrynen</h3>
+                <h5>Käytetyt tunnit: 56h 15m</h5>
                 <ul>
-                    <li>Miten projekti eteni: </li>
-                    <li>Mikä onnistui: </li>
-                    <li>Haasteita: </li>
-                    <li>Tiimin toiminta: </li>
-                    <li>Käytetyt tunnit: </li>
-                    <li>Mitä opit: </li>
+                    <li>Miten projekti eteni: Hitaasti, mutta varmasti</li>
+                    <li>Mikä onnistui: Töiden suunnittelu ja suurin osa tarvittavista toiminnoista</li>
+                    <li>Haasteita: Rekisteröitymisen saaminen käytettäväksi, kirjaa tietokantaan, muttei toimi oikein.</li>
+                    <li>Tiimin toiminta: Yhteistä työskentelyaikaa tuntien ulkopuolella ei saatu aikaan, motivaatio loppu kesken.</li>
+                    <li>Mitä opit: Hacknplanin käyttöä paremmin, php soveltamista ja reactin käyttöä.</li>
                 </ul>
             </div>
             <div id="miika"></div>
