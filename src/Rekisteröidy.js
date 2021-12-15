57bb2ec2e17bd335a8f50b67aea6f716800d58a8
n0vaan00 <n0vaan00@students.oamk.fi>
2021-12-15 14:37:04 +0200

    rekisteröityminen pelittää, väyrynen kuittaa

--- src/Rekisteröidy.js
+++ src/Rekisteröidy.js
@@ -1,54 +1,79 @@
-import React from 'react'
+import React,{useState}  from 'react'
 import './Rekisteröidy.css';
+import axios from 'axios';
+
 
 export default function Rekisteröidy() {
+    const[firstname, setFirstname] = useState("");
+    const[lastname, setLastname] = useState("");
+    const[username, setUsername] = useState("");
+    const[address, setAddress] = useState("");
+    const[zip, setZip] = useState("");
+    const[city, setCity] = useState("");
+    const[password, setPassword] = useState("");
+    const[email, setEmail] = useState("");
+    const[phone, setPhone] = useState("");
+
+
+    const createUser = e =>{
+        e.preventDefault();
+        axios.post("http://localhost/verkkokauppaprojekti-back/customer/addcustomer.php", {
+            firstname: firstname,
+            lastname:lastname,
+            email:email,
+            phone:phone,
+            address:address,
+            zip:zip,
+            city:city,
+            username:username,
+            password:password
+        })
+        .catch(e=> console.log(e))
+    }
+
     return (
     <div id="main">
         <h2>Rekisteröidy</h2>
             <form method="post" action="register.php">
                 <div class="input-group">
                     <label>Etunimi</label>
-                    <input type="text" name="etunimi"></input>
+                    <input value={firstname} onChange={e => setFirstname(e.target.value)}></input>
                 </div>
                 <div class="input-group">
                     <label>Sukunimi</label>
-                    <input type="text" name="sukunimi"></input>
+                    <input value={lastname} onChange={e => setLastname(e.target.value)}></input>
                 </div>
                 <div class="input-group">
                     <label>Osoite</label>
-                    <input type="text" name="osoite"></input>
+                    <input value={address} onChange={e => setAddress(e.target.value)}></input>
                 </div>
                 <div class="input-group">
                     <label>Postinumero</label>
-                    <input type="text" name="postinro"></input>
+                    <input value={zip} onChange={e => setZip(e.target.value)}></input>
                 </div>
                 <div class="input-group">
                     <label>Postitoimipaikka</label>
-                    <input type="text" name="postotmp"></input>
+                    <input value={city} onChange={e => setCity(e.target.value)}></input>
                 </div>
                 <div class="input-group">
                     <label>Puhelinnumero</label>
-                    <input type="text" name="puhnum"></input>
+                    <input value={phone} onChange={e => setPhone(e.target.value)}></input>
                 </div>
                 <div class="input-group">
                     <label>Sähköposti</label>
-                    <input type="email" name="email"></input>
+                    <input value={email} onChange={e => setEmail(e.target.value)}></input>
                 </div>
                 <div class="input-group">
                     <label>Käyttäjänimi</label>
-                    <input type="text" name="username"></input>
+                    <input value={username} onChange={e => setUsername(e.target.value)}></input>
                 </div>
                 
                 <div class="input-group">
                     <label>Salasana</label>
-                    <input type="password" name="password_1"></input>
+                    <input value={password}  onChange={e => setPassword(e.target.value)}></input>
                 </div>
                 <div class="input-group">
-                    <label>Salasanan vahvistus</label>
-                    <input type="password" name="password_2"></input>
-                </div>
-                <div class="input-group">
-                    <button  style={{'color':'white'}}  type="submit" class="btn" name="reg_user">Rekisteröidy</button>
+                    <button  style={{'color':'black'}}  onClick={createUser}>Rekisteröidy</button>
                 </div>
                 <p>
                     Oletko jo rekisteröitynyt? <a href="kirjaudu" style={{'color':'white'}}>Kirjaudu sisään</a>
