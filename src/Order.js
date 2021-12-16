e8548b095ca57d21b0b6a5deb5afcaabdc5b0cf2
n0vaan00 <n0vaan00@students.oamk.fi>
2021-12-16 19:17:53 +0200

    mini

--- src/Order.js
+++ src/Order.js
@@ -134,6 +134,6 @@ export default function Order ({cart, updateAmount, removefromCart, empty, url})
         </div>
     )
   } else {
-    return (<h3 style={{ 'padding-top': '200px' }}>Kiitokset tilauksestasi</h3>);
+    return (<div  style={{ 'padding': '200px', 'padding-bottom': '400px'}}><h3>Paitaässä kiittää tilauksestasi!!!</h3></div>);
   }
 }
