d094b49e2dd9fc4efbf824e8bd6eb9bdb81914ef
Miika Pesonen <n0pemi00.students.oamk.fi>
2021-12-16 14:58:31 +0200

    isompi button

--- src/Product.js
+++ src/Product.js
@@ -14,7 +14,7 @@ export default function Product({product,addToCart,url}) {
     <Col md={{ span: 6, offset: 4 }} className="hinta">Tuotteen hinta: {product?.price}€</Col>
     <Col md={{ span: 4}} className="kuva"><img src={url + 'images/' + product?.image} alt={product?.name}/></Col>
     <Col md={{ span: 6}} className="info">{product?.info}</Col>
-    <Col md={{ span: 3}} className="nappi"><button class="btn btn-primary" type="button" onClick={e => addToCart(product)}>Lisää ostoskoriin</button></Col>
+    <Col md={{ span: 3}} className="nappi"><button class="btn-lg btn-primary" type="button" onClick={e => addToCart(product)}>Lisää ostoskoriin</button></Col>
   </Row>
 </Container>
     </div> 
